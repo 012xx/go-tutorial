d92769ca6ac898cb6a80674b2c5be49823910978
012xx <night.fox.045@gmail.com>
2023-06-30 09:34:05 +0000

    :sparkles: : simple-web-serverをlocalhostで見られるようにした

--- simple-web-server/main.go
+++ simple-web-server/main.go
@@ -6,12 +6,28 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET"
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
 func main() {
